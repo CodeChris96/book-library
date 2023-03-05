7ddb2d997a46ecbcbdc7fc56eaa1827ce2ba4a2a
Chris Edwards <chrisedwards96@outlook.com>
2023-03-05 16:30:19 +0000

    added password onto readers

--- src/models/reader.js
+++ src/models/reader.js
@@ -2,7 +2,12 @@ module.exports = (connection, DataTypes) => {
   const schema = {
     name: DataTypes.STRING,
     email: DataTypes.STRING,
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   };
+
   const ReaderModel = connection.define("Reader", schema);
   return ReaderModel;
 };
